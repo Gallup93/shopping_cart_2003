1af057176d05663b546803b380cc084f55e70d32
Michael Gallup <Gallup93@icloud.com>
2020-03-25 09:41:46 -0600

    added initialization, corresponding test method

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def test_initialization
+    cart = ShoppingCart.new("King Soopers", 30)
+    assert_equal 'King Soopers', cart.name
+    assert_equal 30, cart.capacity
+    assert_equal [], cart.products
+  end
+
+end
