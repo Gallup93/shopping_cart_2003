e27f7b02e2799a7b141c1290386abb146ae12d53
Michael Gallup <Gallup93@icloud.com>
2020-03-25 09:48:01 -0600

    added add_product method, corresponding test method

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -12,4 +12,14 @@ class ShoppingCartTest < Minitest::Test
     assert_equal [], cart.products
   end
 
+  def test_add_product
+    cart = ShoppingCart.new("King Soopers", 30)
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal [product1, product2], cart.products
+  end
+
 end
